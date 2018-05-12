efd29e5f93537c2c2550487378d23bf662e68bc4
Keloo <vlad-negura@mail.ru>
2018-05-12 13:02:51 +0300

    adding bind/unbind methods to vertex array class

--- src/Framework/VertexArray.cpp
+++ src/Framework/VertexArray.cpp
@@ -2,13 +2,28 @@
 #include "Framework/VertexArray.hpp"
 
 namespace Framework {
-    VertexArray::VertexArray(){};
-    VertexArray::~VertexArray(){};
+    VertexArray::VertexArray(){
+        GLCall(glGenVertexArrays(1, &glVertexArrayId));
+    };
+    VertexArray::~VertexArray(){
+        GLCall(glDeleteVertexArrays(1, &glVertexArrayId));        
+    };
+
+    void VertexArray::bind() const {
+        GLCall(glBindVertexArray(glVertexArrayId));
+    }
+
+    void VertexArray::unbind() const {
+        GLCall(glBindVertexArray(0));        
+    }
 
     void VertexArray::addBuffer(const VertexBuffer &buffer, const VertexBufferLayout &layout) {
+        bind();
         buffer.bind();
+
         const std::vector<VertexBufferElement> &elements = layout.getElements();
         unsigned int offset = 0;
+        
         for (unsigned int i=0; i<elements.size(); i++) {
             const VertexBufferElement &element = elements[i];
             GLCall(glEnableVertexAttribArray(i));
